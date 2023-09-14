b75dfda02d6a3ceeb198008ee46cbda84e02eae7
Nehal-Azure <nehalaz400@outlook.com>
2023-09-14 09:10:08 +0100

    First Commit

---
+++ src/test/java/tests/FirstTest1.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest1 {
+    @Test
+    public void testDemo1(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String actualUrl = driver.getCurrentUrl();
+        System.out.println("Current Url is found as: "+driver.getCurrentUrl());
+        Assert.assertEquals(actualUrl,expectedUrl);
+        driver.quit();
+
+
+
+    }
+}
